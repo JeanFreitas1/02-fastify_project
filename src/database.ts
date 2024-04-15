8bfd0d8552b22b1406604a5f1702ed6f717aba04
JeanFreitas1 <jean-freita@hotmail.com>
2024-04-15 07:47:37 -0300

    added knex configuration file

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,12 @@
 // eslint-disable-next-line import/no-named-default
 import { default as knexSetup } from "knex"
 
-export const knex = knexSetup({
+export const configDatabase = {
   client: "sqlite3",
   connection: {
     filename: "./tmp/database.db",
   },
-})
+  useNullAsDefault: true,
+}
+
+export const knex = knexSetup(configDatabase)
