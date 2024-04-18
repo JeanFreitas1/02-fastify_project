8bdfcfb81b36b0bb783859482694255d5642c805
JeanFreitas1 <jean-freita@hotmail.com>
2024-04-18 06:03:18 -0300

    added zod and .env index.ts file

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,11 @@
 // eslint-disable-next-line import/no-named-default
 import { default as knexSetup, Knex } from "knex"
-import "dotenv/config"
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL env not found!")
-}
+import { env } from "./env"
 
 export const configDatabase: Knex.Config = {
   client: "sqlite3",
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
