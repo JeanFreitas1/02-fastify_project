8d95821a5b4b133f947e03f9aca2b0498fdecb60
JeanFreitas1 <jean-freita@hotmail.com>
2024-04-21 16:40:56 -0300

    added new host configuration for production

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import { app } from "./app"
 import { env } from "./env"
 
-app.listen({ port: env.PORT }, () => {
-  console.log(`Server running on port ${env.PORT}...`)
-})
+app.listen(
+  {
+    port: env.PORT,
+    host: env.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
+  },
+  () => {
+    console.log(`Server running on port ${env.PORT}...`)
+  },
+)
